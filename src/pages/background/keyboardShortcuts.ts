11b801cd4ad513e969aacb80054ef807bbf1002d
Bennett <hello@vantezzen.io>
2022-07-02 21:21:45 +0200

    Refactor keyboard shortcuts into separate file

---
+++ src/pages/background/keyboardShortcuts.ts
@@ -0,0 +1,21 @@
+import { browser } from 'webextension-polyfill-ts';
+
+export default function setupKeyboardShortcutsListener() {
+  // React to keyboard shortcuts
+  // We simply redirect them to the page using a browser message
+  browser.commands.onCommand.addListener(async (name: String) => {
+    const tabs = await browser.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    if (!tabs[0] || !tabs[0].id) {
+      // We can't connect to a page
+      return;
+    }
+
+    await browser.tabs.sendMessage(tabs[0].id, {
+      command: 'shortcut',
+      name,
+    });
+  });
+}
