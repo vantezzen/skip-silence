d7d25c6c807464475a186409601c3418f0730671
Bennett <hello@vantezzen.io>
2022-08-21 15:31:07 +0200

    Improve firefox support

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -4,6 +4,7 @@
  */
 import { StateEnvironment } from "@vantezzen/plasmo-state"
 import cssText from "data-text:./lib/content.styles.css"
+import debugging from "debug"
 import type { PlasmoContentScript } from "plasmo"
 import browser from "webextension-polyfill"
 
@@ -16,6 +17,8 @@ import Bar from "./lib/command-bar/Bar"
 import "./lib/content.styles.css"
 import setupKeyboardShortcuts from "./lib/keyboardShortcuts"
 
+const debug = debugging("skip-silence:contents:index")
+
 export const config: PlasmoContentScript = {
   matches: ["<all_urls>"],
   all_frames: true
@@ -25,6 +28,7 @@ const state = getState(StateEnvironment.Content)
 
 setupBrowserContent(state)
 if (!supportsTabCapture) {
+  debug("Doesn't support tabCapture")
   setupFirefoxContent(state)
 }
 
